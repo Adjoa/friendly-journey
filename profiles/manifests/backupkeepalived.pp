280f6628e7e02421f91079d30df9ad6b0860e5de
root <root@puppet.example.com>
2015-12-18 04:52:43 +0000

    first commit

---
+++ profiles/manifests/backupkeepalived.pp
@@ -0,0 +1,23 @@
+class profiles::backupkeepalived    {
+
+include keepalived
+
+keepalived::vrrp::script { 'check_haproxy':
+    script => 'killall -0 haproxy',
+  }
+
+  keepalived::vrrp::instance { 'VI_50':
+    interface         => 'eth1',
+    state             => 'BACKUP',
+    virtual_router_id => '50',
+    priority          => '100',
+    auth_type         => 'PASS',
+    auth_pass         => 'secret',
+    virtual_ipaddress => [ '10.80.15.109' ],
+    track_interface   => ['eth1','tun0'], # optional, monitor these interfaces.
+    track_script      => 'check_haproxy',  
+    unicast_peers     => ['10.80.15.103', '10.80.15.109']
+}
+
+}
+
---
+++ profiles/manifests/masterkeepalived.pp
@@ -0,0 +1,24 @@
+class profiles::masterkeepalived    {
+
+include keepalived
+
+keepalived::vrrp::script { 'check_haproxy':
+    script => 'killall -0 haproxy',
+  }
+
+
+  keepalived::vrrp::instance { 'VI_50':
+    interface         => 'eth1',
+    state             => 'MASTER',
+    virtual_router_id => '50',
+    priority          => '101',
+    auth_type         => 'PASS',
+    auth_pass         => 'secret',
+    virtual_ipaddress => [ '10.80.15.109' ],
+    track_interface   => ['eth1','tun0'], # optional, monitor these interfaces.
+    track_script      => 'check_haproxy',  
+    unicast_peers     => ['10.80.15.104', '10.80.15.109']
+
+}
+
+}
