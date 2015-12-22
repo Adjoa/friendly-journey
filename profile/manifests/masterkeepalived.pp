38174dd3e885a44603c2e3270f137cd5af349513
Adjoa <adjoa.darien@gmail.com>
2015-12-22 13:54:00 -0500

    Added comments to masterkeepalived.pp

--- profile/manifests/masterkeepalived.pp
+++ profile/manifests/masterkeepalived.pp
@@ -1,11 +1,18 @@
+# Submitted by: Adjoa Darien
+# Last updated: Dec-22-2015
+# Configures Keepalived (/etc/keepalived/keepalived.conf) to set up two 
+# load balancer nodes in an active-passive configuration with HTTP failover. 
 class profile::masterkeepalived{
   include keepalived
-
+  
+  # Checks whether the haproxy service is running
   keepalived::vrrp::script { 'check_haproxy':
     script => 'killall -0 haproxy',
   }
-
-
+  
+  # Defines this load balancer as the MASTER in the active-passive configuration.
+  # - unicast_peers parameter contains an array of IP addresses that correspond
+  #   to the failover nodes.
   keepalived::vrrp::instance { 'VI_50':
     interface         => 'eth1',
     state             => 'MASTER',
