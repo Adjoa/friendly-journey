f66bdb70f7ff71c50a7ac4594cee7a25277a19ef
Adjoa <adjoa.darien@gmail.com>
2015-12-22 11:23:29 -0500

    Updated comments for vhost.pp

--- profile/manifests/vhost.pp
+++ profile/manifests/vhost.pp
@@ -1,27 +1,32 @@
+# Defines a virtual host and prepares the web server to communicate with a
+# transparent HAProxy server.
 class profile::vhost ( 
   $portnum = hiera('apacheport'), 
   $docroot = $::apache::params::docroot, 
 ){
 
-  # include apache
+  # default_vhost setting allows for the creation of customized Apache virtual hosts
+  # log_formats setting instructs the web server to log the ip address of the requesting
+  # client (instead of the proxy's ip address) which is being passed to it from the HTTP
+  # header field %{X-Forwarded-For}i
   class { 'apache':
     default_vhost => false,
     log_formats   => { 
       combined => '%{X-Forwarded-For}i %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"'},
   }
 
-  # set the port and modify the logs
+  # HAProxy will regularly check on the server's health by requesting the file check.txt
+  file { "${docroot}/check.txt":
+     ensure => present,
+  }
+  
+  # Configures a name-based virtual host with the hostname first.example.com and,
+  # instructs the server not to log requests for the file check.txt
   apache::vhost { 'first.example.com':
     port               => $portnum,
     docroot            => '/var/www/first',
     access_log_format  => 'combined',
-    setenvif           => ['Request_URI "^/check\.txt$" dontlog',],
     access_log_env_var => "!dontlog",
-
-  }
-
-  # create the file check.txt to put the logs in
-  file { "${docroot}/check.txt":
-     ensure => present,
+    setenvif           => ['Request_URI "^/check\.txt$" dontlog',],
   }
 }
